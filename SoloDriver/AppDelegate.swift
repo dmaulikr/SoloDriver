f241949119ee082f7445db7edc9991e2618e7d07
HBJ <haoboji@gmail.com>
2016-08-15 01:09:47 +1000

    add current location button

--- SoloDriver/AppDelegate.swift
+++ SoloDriver/AppDelegate.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import GoogleMaps
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,7 +15,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-        GMSServices.provideAPIKey("AIzaSyCkYKPAn1h-uKh-_xIdd1kB3bCfDk4AHck")
         LocationService.shared.start()
         return true
     }
--- SoloDriver/MapController.swift
+++ SoloDriver/MapController.swift
@@ -7,36 +7,46 @@
 //
 
 import UIKit
-import GoogleMaps
+import MapKit
 
 class MapController: UIViewController {
 
-    var mapView: GMSMapView?
+    @IBOutlet var mapView: MKMapView!
+
+    @IBOutlet var navItem: UINavigationItem!
+    @IBOutlet var navBar: UINavigationBar!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Default location - Melbourne
-        let camera: GMSCameraPosition
+
         let lastLocation = LocationService.shared.getLastLocation()
-        if (lastLocation != nil) {
-            camera = GMSCameraPosition.cameraWithLatitude(
-                lastLocation!.coordinate.latitude, longitude: lastLocation!.coordinate.longitude, zoom: 10.0)
-        } else {
-            camera = GMSCameraPosition.cameraWithLatitude(-37.768356, longitude: 144.9663673, zoom: 8.0)
-        }
-        mapView = GMSMapView.mapWithFrame(self.view.bounds, camera: camera)
-        mapView!.myLocationEnabled = true
-        mapView!.settings.compassButton = true
-        mapView!.settings.myLocationButton = true
-        mapView!.padding = UIEdgeInsets(top: 80.0, left: 0.0, bottom: 60.0, right: 0.0)
-        self.view.insertSubview(mapView!, atIndex: 0)
+
+        let currentLocationItem = MKUserTrackingBarButtonItem(mapView: mapView)
+        navBar.topItem!.rightBarButtonItem = currentLocationItem
+        navBar.setBackgroundImage(UIImage(), forBarMetrics: UIBarMetrics.Default)
+        navBar.shadowImage = UIImage()
+        navBar.translucent = true
+        navBar.backgroundColor = UIColor.clearColor()
+//        if (lastLocation != nil) {
+//            camera = GMSCameraPosition.cameraWithLatitude(
+//                lastLocation!.coordinate.latitude, longitude: lastLocation!.coordinate.longitude, zoom: 10.0)
+//        } else {
+//            camera = GMSCameraPosition.cameraWithLatitude(-37.768356, longitude: 144.9663673, zoom: 8.0)
+//        }
+//        mapView = GMSMapView.mapWithFrame(self.view.bounds, camera: camera)
+//        mapView!.myLocationEnabled = true
+//        mapView!.settings.compassButton = true
+//        mapView!.settings.myLocationButton = true
+//        mapView!.padding = UIEdgeInsets(top: 80.0, left: 0.0, bottom: 60.0, right: 0.0)
+//        self.view.insertSubview(mapView!, atIndex: 0)
     }
 
     @IBAction func searchThisArea(sender: UIButton) {
-        let cameraPosition = mapView!.camera.target
-        PublicDataService.getHMLRoute(cameraPosition) { (result) in
-            print(result)
-        }
+//        let cameraPosition = mapView!.camera.target
+//        PublicDataService.getHMLRoute(cameraPosition) { (result) in
+//            print(result)
+//        }
     }
 
     override func didReceiveMemoryWarning() {
